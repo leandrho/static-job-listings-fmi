74dff1fda52ce42f5301e2049fbf2d4dbfe5cb4c
Leandrho <leandro.coder01@gmail.com>
2025-01-24 12:33:37 -0300

    add mains

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header, Main } from './components';
+
+function App() {
+  return (
+    <div className='h-svh w-full '>
+      <Header />
+      <Main />
+    </div>
+
+  )
+}
+
+export default App
