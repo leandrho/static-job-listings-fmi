8ea44b73d91b97d4ad8e6691f66f98104bb28dca
Leandrho <leandro.coder01@gmail.com>
2025-01-25 18:08:51 -0300

    using context

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { JobsProvider } from './context/JobsProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <JobsProvider>
+      <App />
+    </JobsProvider>
   </StrictMode>,
 )
