87ac43b076fe6a2e0bd9ba7a4815f938a503d550
Leandrho <leandro.coder01@gmail.com>
2025-01-27 11:01:34 -0300

    fix header bg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 
 export const Header = () => {
   return (
-    <header className="h-[156px] w-full bg-[url(./images/bg-header-mobile.svg)] md:bg-[url(./images/bg-header-desktop.svg)] bg-cover bg-no-repeat bg-(--Desaturated-DarkCyan)">
+    <header className="h-[156px] w-full headerbg bg-cover bg-no-repeat bg-(--Desaturated-DarkCyan)">
     </header>
   )
 }
